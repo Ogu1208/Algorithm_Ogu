496d2901c5f1fe4700867b517ddd4f3f68672ad9
Ogu1208 <kdasunb6@gmail.com>
2025-03-10 13:53:25 +0900

    [그리디] LeetCode - 주식을 사고팔기 좋은 가장 좋은 지점 2 풀이 개선

--- Leetcode/src/그리디/Q122_BestTimeToBuyAndSellStock2.java
+++ Leetcode/src/그리디/Q122_BestTimeToBuyAndSellStock2.java
@@ -22,17 +22,15 @@ public class Q122_BestTimeToBuyAndSellStock2 {
     }*/
 
     static int maxProfit(int[] prices) {
-        int result = 0;
+        int profit = 0;
 
-        // 전체를 순회하며 다음번 값이 오르는 경우를 찾는다.
-        for (int i = 0; i < prices.length - 1; i++) {
-            // 다음번 값이 오르는 경우 항상 이익을 취한다.
-            if (prices[i + 1] > prices[i]) {
-                result += prices[i + 1] - prices[i];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
             }
         }
 
-        return result;
+        return profit;
     }
 
     public static void main(String[] args) {
