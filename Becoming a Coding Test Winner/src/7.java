c35d54d76270ea40804059cea99c2b445ab0d1fd
Ogu1208 <kdasunb6@gmail.com>
2025-12-10 18:34:04 +0900

    [배열] CTWinner - Q7

--- Becoming a Coding Test Winner/src/7.java
+++ Becoming a Coding Test Winner/src/7.java
@@ -1,9 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 class Solution7 {
 
-	public int[] solution(int[] answers) {
-		return null;
+	static final HashMap<Character, int[]> dir = new HashMap<>();
+
+	static void initLocation() {
+		dir.put('U', new int[]{0, 1});
+		dir.put('D', new int[]{0, -1});
+		dir.put('R', new int[]{1, 0});
+		dir.put('L', new int[]{-1, 0});
+	}
+
+	public int solution(String dirs) {
+		initLocation();
+		int x = 5, y = 5;
+		HashSet<String> answer = new HashSet<>();
+
+		for(int i = 0; i < dirs.length(); i++) {
+			int[] offset = dir.get(dirs.charAt(i));
+
+			int nx = x + offset[0];
+			int ny = y + offset[1];
+
+			if(!rangeCheck(nx, ny)) continue;
+
+			answer.add(x + " " + y + " " + nx + " " + ny);
+			answer.add(nx + " " + ny + " " + x + " " + y);
+
+			x = nx;
+			y = ny;
+		}
+
+		return answer.size() / 2;
+	}
+
+	static boolean rangeCheck(int nx, int ny) {
+		return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
 	}
 }
