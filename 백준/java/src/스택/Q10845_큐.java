dd642d67bb5a93fbb191d77f33f82e5ab3dc6c88
Ogu1208 <kdasunb6@gmail.com>
2026-01-27 12:50:20 +0900

    [큐] 백준 - Q18258_큐2

---
+++ 백준/java/src/스택/Q10845_큐.java
@@ -0,0 +1,50 @@
+package 스택;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Q10845_큐 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+		for (int i = 0; i <N; i++) {
+			String command = br.readLine();
+
+			if (command.startsWith("push")) {
+				int x = Integer.parseInt(command.split(" ")[1]);
+				deque.addLast(x);
+			} else if (command.startsWith("pop")) {
+				if (deque.isEmpty()) {
+					sb.append(-1).append('\n');
+				} else {
+					sb.append(deque.pollFirst()).append('\n');
+				}
+			} else if (command.startsWith("size")) {
+				sb.append(deque.size()).append('\n');
+			} else if (command.startsWith("empty")) {
+				sb.append(deque.isEmpty() ? 1 : 0).append('\n');
+			} else if (command.startsWith("front")) {
+				if (deque.isEmpty()) {
+					sb.append(-1).append('\n');
+				} else {
+					sb.append(deque.peekFirst()).append('\n');
+				}
+			} else if (command.startsWith("back")) {
+				if (deque.isEmpty()) {
+					sb.append(-1).append('\n');
+				} else {
+					sb.append(deque.peekLast()).append('\n');
+				}
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
---
+++ 백준/java/src/스택/Q18258_큐2.java
@@ -0,0 +1,48 @@
+package 스택;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Q18258_큐2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		Deque<Integer> deque = new ArrayDeque<>();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+
+			switch (cmd) {
+				case "push":
+					int x = Integer.parseInt(st.nextToken());
+					deque.offer(x);
+					break;
+				case "pop":
+					sb.append(deque.isEmpty() ? -1 : deque.poll()).append('\n');
+					break;
+				case "size":
+					sb.append(deque.size()).append('\n');
+					break;
+				case "empty":
+					sb.append(deque.isEmpty() ? 1 : 0).append('\n');
+					break;
+				case "front":
+					sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append('\n');
+					break;
+				case "back":
+					sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append('\n');
+					break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
