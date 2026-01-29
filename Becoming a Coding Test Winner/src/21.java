069e5583c1be31ff4102a2477a5c3866bbfdbbb6
Ogu1208 <kdasunb6@gmail.com>
2026-01-29 15:34:29 +0900

    [해시] CTWinner - Q21. 오픈 채팅방

---
+++ Becoming a Coding Test Winner/src/21.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class Solution21 {
+
+	public String[] solution(String[] record) {
+		ArrayList<String> answer = new ArrayList<>();
+
+		HashMap<String, String> uidNameMap = new HashMap<>();
+
+		// 1. uid -> nickname 매핑
+		for (String offset : record) {
+			StringTokenizer st = new StringTokenizer(offset);
+
+			String command = st.nextToken();
+			String uid = st.nextToken();
+
+			if (command.equals("Enter") || command.equals("Change")) {
+				String nickname = st.nextToken();
+				uidNameMap.put(uid, nickname);
+			}
+		}
+
+		// 2 . 메시지 생성
+		for (String offset : record) {
+			StringTokenizer st = new StringTokenizer(offset);
+
+			String command = st.nextToken();
+			String uid = st.nextToken();
+
+			if (command.equals("Enter")) {
+				answer.add(uidNameMap.get(uid) + "님이 들어왔습니다.");
+			} else if (command.equals("Leave")) {
+				answer.add(uidNameMap.get(uid) + "님이 나갔습니다.");
+			}
+		}
+
+		return answer.toArray(new String[0]);
+	}
+}
