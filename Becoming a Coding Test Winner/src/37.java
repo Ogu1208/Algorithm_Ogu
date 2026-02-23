997518739f8043c9b6240d4b6a25bb3d67b85fb5
Ogu1208 <kdasunb6@gmail.com>
2026-02-24 04:06:21 +0900

    [BFS] CTWinner - 게임 맵 최단 거리

---
+++ Becoming a Coding Test Winner/src/37.java
@@ -0,0 +1,55 @@
+import java.util.ArrayDeque;
+
+class Solution {
+
+	private static final int[] dirX = {0, 0, 1, -1};
+	private static final int[] dirY = {1, -1, 0, 0};
+
+	public static class Node {
+		int r, c;
+
+		public Node(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+
+	public int solution(int[][] maps) {
+		int N = maps.length;
+		int M = maps[0].length;
+
+		// 최단 거리 저장
+		int[][] dist = new int[N][M];
+
+		ArrayDeque<Node> queue = new ArrayDeque<>();
+		queue.addLast(new Node(0, 0));
+		dist[0][0] = 1;
+
+		while (!queue.isEmpty()) {
+			Node now = queue.pollFirst();
+
+			for (int i = 0; i < 4; i++) {
+				int nr = now.r + dirX[i];
+				int nc = now.c + dirY[i];
+
+				// 맵 밖으로 나가는 경우
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M) {
+					continue;
+				}
+
+				// 벽인 경우
+				if (maps[nr][nc] == 0) {
+					continue;
+				}
+
+				// 이동한 위치가 처음 방문하느 경우, queue에 추가하고 거리 갱신
+				if (dist[nr][nc] == 0) {
+					queue.addLast(new Node(nr, nc));
+					dist[nr][nc] = dist[now.r][now.c] + 1;
+				}
+			}
+		}
+
+		return dist[N - 1][M - 1] == 0 ? -1 : dist[N - 1][M - 1];
+	}
+}
--- Becoming a Coding Test Winner/src/Main.java
+++ Becoming a Coding Test Winner/src/Main.java
@@ -1,70 +1,56 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
 	public static void main(String[] args) {
-		int[][] operations1 = {{0, 0, 1}, {0, 1, 2}, {1, 1, 2}};
-		System.out.println(Arrays.toString(solution(3, operations1)));
-		int[][] operations2 = {{0, 0, 1}, {1, 1, 2}, {0, 1, 2}, {1, 0, 2}};
-		System.out.println(Arrays.toString(solution(3, operations2)));
-
-		// String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
-		// String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
-		// String[] seller = {"young", "john", "tod", "emily", "mary"};
-		// int[] amount = {12, 4, 2, 5, 10};
-		//
-		// System.out.println(Arrays.toString(solution2(enroll, referral, seller, amount)));
+		int[][] graph1 = {{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 6}, {3, 7}, {4, 8}, {5, 8}, {6, 9}, {7, 9}};
+		System.out.println(Arrays.toString(solution(graph1, 1, 9)));
+		int[][] graph2 = {{1, 3}, {3, 4}, {3, 5}, {5, 2}};
+		System.out.println(Arrays.toString(solution(graph2, 1, 5)));
 	}
 
-	private static int[] parent;
+	private static ArrayList<Integer>[] adjList;
+	private static boolean[] visited;
+	private static ArrayList<Integer> answer;
 
-	private static Boolean[] solution(int k, int[][] operation) {
-		parent = new int[k];
-
-		for (int i = 0; i < k; i++) {
-			parent[i] = i;
+	private static int[] solution(int[][] graph, int start, int n) {
+		adjList = new ArrayList[n + 1];
+		for (int i = 0; i < adjList.length; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 
-		ArrayList<Boolean> answer = new ArrayList<>();
+		visited = new boolean[n + 1];
+		answer = new ArrayList<>();
 
-		// O(NlogN)
-		for (int[] op : operation) {
-			if (op[0] == 0) { // union 연산
-				union(op[1], op[2]);
-			} else {    // 같은 집합 equals 연산
-				answer.add(find(op[1]) == find(op[2]));
+		for (int[] edge : graph) {
+			adjList[edge[0]].add(edge[1]);
+		}
+
+		bfs(start);
+
+		return answer.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	private static void bfs(int start) {
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+		queue.addLast(start);
+		visited[start] = true;
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			answer.add(now);
+
+			for(int next : adjList[now]) {
+				if (!visited[next]) {
+					queue.addLast(next);
+					visited[next] = true;
+				}
 			}
 		}
-
-		return answer.toArray(new Boolean[0]);
 	}
 
-	// O(logN)
-	private static int find(int x) {
-		if (parent[x] == x) {
-			return x;
-		}
-
-		return find(parent[x]);
-	}
-
-	// O(logN)
-	private static void union(int x, int y) {
-		int rootx = find(x);
-		int rooty = find(y);
-
-		parent[y] = rootx;
-	}
 }
