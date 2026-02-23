99388ba27fb929872dbc376822e5aa94208ee433
Ogu1208 <kdasunb6@gmail.com>
2026-02-24 04:06:21 +0900

    [다익스트라] CTWinner - Q40. 배달

--- Becoming a Coding Test Winner/src/Main.java
+++ Becoming a Coding Test Winner/src/Main.java
@@ -1,59 +1,64 @@
-import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
 	public static void main(String[] args) {
+		int[][] graph = {{0, 1, 9}, {0, 2, 3}, {1, 0, 5}, {2, 1, 1}};
+		System.out.println(Arrays.toString(solution(graph, 0, 3)));
+		int[][] graph2 = {{0, 1, 1}, {1, 2, 5}, {2, 3, 1}};
+		System.out.println(Arrays.toString(solution(graph2, 0, 4)));
 	}
 
-	// 상, 하, 좌, 우
-	private static final int[] dirR = {0, 0, 1, -1};
-	private static final int[] dirC = {1, -1, 0, 0};
+	private static class Node {
+		int dest, cost;
 
-	public static class Node {
-		int r, c;
-
-		public Node(int r, int c) {
-			this.r = r;
-			this.c = c;
+		public Node(int dest, int cost) {
+			this.dest = dest;
+			this.cost = cost;
 		}
 	}
 
-	public int solution(int[][] maps) {
-		int N = maps.length;
-		int M = maps[0].length;
+	// 이 부분을 변경해서 실행해보세요.
+	public static int[] solution(int[][] graph, int start, int n) {
+		ArrayList<Node>[] adjList = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			adjList[i] = new ArrayList<>();
+		}
 
-		// 최단 거리 저장
-		int[][] dist = new int[N][M];
+		for (int edge[] : graph) {
+			adjList[edge[0]].add(new Node(edge[1], edge[2]));
+		}
 
-		ArrayDeque<Node> queue = new ArrayDeque<>();
-		queue.addLast(new Node(0, 0));
-		dist[0][0] = 1;
+		int[] dist = new int[n]; // 거리 저장할 배열
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		boolean[] visited = new boolean[n]; // 방문 여부를 저장할 배열
 
-		while (!queue.isEmpty()) {
-			Node now = queue.pollFirst();
+		dist[start] = 0; // 시작 노드의 거리 값은 0으로 초기화
 
-			for (int i = 0; i < 4; i++) {
-				int nr = now.r + dirR[i];
-				int nc = now.c + dirC[i];
+		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+		pq.add(new Node(start, 0));
 
-				if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
-					continue;
-				}
+		while (!pq.isEmpty()) {
+			Node now = pq.poll();
 
-				if (maps[nr][nc] == 0) {
-					continue;
-				}
+			if (visited[now.dest])
+				continue;
 
-				if (dist[nr][nc] == 0) {
-					queue.addLast(new Node(nr, nc));
-					dist[nr][nc] = dist[now.r][now.c] + 1; // 방문 처리
+			visited[now.dest] = true;
+
+			for (Node next : adjList[now.dest]) {
+				if (dist[next.dest] > now.cost + next.cost) {
+					dist[next.dest] = now.cost + next.cost;
+					pq.add(new Node(next.dest, dist[next.dest]));
 				}
 			}
 		}
 
-		return dist[N - 1][M - 1] == 0 ? -1 : dist[N - 1][M - 1];
+		return dist;
 	}
 
 }
